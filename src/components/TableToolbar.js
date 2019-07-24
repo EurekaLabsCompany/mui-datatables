01fedecc4c81921ab1573a08c96a237227e49ba3
João Luiz Ramalheira <jlramalheira@gmail.com>
2019-07-24 10:07:37 -0300

    Print in landscape format and A4 size

--- src/components/TableToolbar.js
+++ src/components/TableToolbar.js
@@ -220,6 +220,7 @@ class TableToolbar extends React.Component {
                   </Tooltip>
                 )}
                 content={() => this.props.tableRef()}
+                pageStyle={'@media print {html {width: 297mm; height: 210mm;} @page {size: landscape}}'}
               />
             </span>
           )}
